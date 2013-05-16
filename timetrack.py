a88bf426ceeeb7bf5d0783e1e794de1823c37e72
vnavarro <vnavarro@iurbotica.com>
2013-05-16 09:08:04 +0200

    log to sqlite

--- timetrack.py
+++ timetrack.py
@@ -1,28 +1,48 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*- 
 
-import logging
 from subprocess import check_output, CalledProcessError
 import threading 
 import time
-
-logfile = 'timetrack.csv'
+from datetime import datetime
+import os
+import sqlite3
 
 
 class CheckActiveWindow (threading.Thread):
-    def __init__(self):
+		def __init__(self):
+				self.cur = None
+				self.conn = None
 				self.stopped = False
 				self.new = dict()
 				self.last = dict()
-				logging.basicConfig(filename=logfile, format='%(asctime)s, %(message)s', datefmt='%m/%d/%Y %H:%M:%S', level=logging.DEBUG)
 				threading.Thread.__init__(self)
 
-    def check(self):
+		def initdb(self):
+				# data
+				home = os.path.expanduser("~")
+				database = home + "/timetrack.sqlite"
+
+				if not os.path.exists(database):
+						open(database, 'w').close()
+				self.conn = sqlite3.connect(database)
+				self.cur = self.conn.cursor()
+				sql = """
+						CREATE TABLE IF NOT EXISTS records ( \
+								date TEXT, \
+								host TEXT, \
+								class TEXT, \
+								title TEXT\
+						)"""
+				self.cur.execute(sql)
+
+		def check(self):
+				# TODO: take care about screensaver
+				
 				# get active window
 				try:
 						xprop_active_window_info = check_output("nice -n 19 xprop -root _NET_ACTIVE_WINDOW", shell=True)
 				except CalledProcessError, e:
-						logging.info('"Error","CalledProcessError","' + e.output + '"')
+						print('"Error","CalledProcessError","' + e.output + '"')
 						return
 				
 				window = xprop_active_window_info.split()[-1]				
@@ -31,7 +51,7 @@ class CheckActiveWindow (threading.Thread):
 				try:
 					xprop_window_info = check_output("nice -n 19 xprop -id " + window, shell=True)
 				except CalledProcessError, e:
-						logging.info('"Error","CalledProcessError","' + e.output + '"')
+						print('"Error","CalledProcessError","' + e.output + '"')
 						return
 				
 				keyvalues = {}
@@ -40,11 +60,11 @@ class CheckActiveWindow (threading.Thread):
 						keyvalues[name.strip()] = var
 
 				try:
-						self.new['hostname'] = keyvalues['WM_CLIENT_MACHINE(STRING)'].split('"')[1::2][0]
-						self.new['appclass'] =  keyvalues['WM_CLASS(STRING)'].split(',')[1].split('"')[1::2][0]
-						self.new['title'] = keyvalues['_NET_WM_NAME(UTF8_STRING)'].split('"')[1::2][0]
+						self.new['hostname'] = keyvalues['WM_CLIENT_MACHINE(STRING)'].split('"')[1::2][0].decode('utf-8')
+						self.new['appclass'] =  keyvalues['WM_CLASS(STRING)'].split(',')[1].split('"')[1::2][0].decode('utf-8')
+						self.new['title'] = keyvalues['WM_NAME(STRING)'].split('"')[1::2][0].decode('utf-8')
 				except KeyError, e:
-						#logging.info('"Error"' + ',"KeyError:","' + str(e) + '"')
+						print('"Error"' + ',"KeyError:","' + str(e) + '"')
 						self.new['hostname'] = ""
 						self.new['appclass'] = ""
 						self.new['appclass'] = ""
@@ -54,14 +74,14 @@ class CheckActiveWindow (threading.Thread):
 						self.last['hostname'] = self.new['hostname']
 						self.last['appclass'] = self.new['appclass']
 						self.last['title'] = self.new['title']
-						# TODO: take care about screensaver
-						logging.info('"' + self.last['hostname'] + '","' + self.last['appclass'] + '","' + self.last['title'] + '"')
+						self.cur.execute("""INSERT INTO records VALUES (?,?,?,?)""", (datetime.now().strftime("%Y-%m-%d %H:%M:%S"), self.last['hostname'], self.last['appclass'], self.last['title']))
+						self.conn.commit()
 
-    def run(self):
-        while not self.stopped:
-            # call a function
-            self.check()
-            time.sleep(5)
+		def run(self):
+				self.initdb()
+				while not self.stopped:
+						self.check()
+						time.sleep(5)
 
 
 if __name__ == "__main__":
